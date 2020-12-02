21f7eb86b352298223b095ccdb6de40d614dd9ea
longxiangfu <1565267514@qq.com>
2020-12-02 16:18:00 +0800

    爬去up100主的视频信息

---
+++ python/pachong/up_100/up_100.py
@@ -0,0 +1,186 @@
+"""
+bilibili的up100主视频解析
+"""
+import requests
+import os
+import json
+import traceback
+
+
+dir_path = 'up_100'
+
+def get_http_session(pool_connections=2, pool_maxsixe=10, max_retries=3):
+    """
+    创建http连接池
+    :param pool_connections:
+    :param pool_maxsixe:
+    :param max_retries:
+    :return:
+    """
+    # 获取session
+    session = requests.session()
+    # 获取适配器
+    adapter = requests.adapters.HTTPAdapter(
+        pool_connections=pool_connections,
+        pool_maxsize=pool_maxsixe,
+        max_retries=max_retries
+    )
+    # session安装适配器
+    session.mount('http://', adapter)
+    session.mount('https://', adapter)
+    return session
+
+
+def save_file(filepath, content):
+    """
+    保存文件
+    :param filepath:
+    :param content:
+    :return:
+    """
+    with open(filepath, 'a', encoding='utf-8') as f:
+        f.write(content)
+
+def make_dir(name):
+    """
+    创建目录
+    :param name:
+    :return:
+    """
+    up_dir = os.path.join(dir_path, name)
+    if not os.path.exists(up_dir):
+        os.makedirs(up_dir)
+    return up_dir
+
+def log(content, level, filepath):
+    """
+    日志记录
+    :param content:
+    :param level:
+    :param filepath:
+    :return:
+    """
+    with open(filepath, 'a', encoding='utf-8') as f:
+        if level == 'error':
+            f.write(content)
+        if level == 'fail':
+            f.write(content)
+
+def read_json(filepath):
+    """
+    读取json文件
+    :param filepath:
+    :return:
+    """
+    with open(filepath, 'r', encoding='utf-8') as f:
+        res = f.read()
+    return json.loads(res)  # 将res转成字典
+
+def base_info_task(power_json):
+    for d in power_json:
+        uid = d['uid']
+        name = d['name']
+        get_up_base_info(name, uid)
+
+
+def get_up_base_info(name, uid):
+    try:
+        url = f'https://api.bilibili.com/x/space/arc/search?mid={uid}&pn=1&ps=25&order=click&jsonp=jsonp'
+        r = get_http_session().get(url, timeout=100)
+        if r.status_code == 200:
+            up_dir = make_dir(name)
+            filepath = os.path.join(up_dir, f'{uid}_base_info.json')
+            content = json.dumps(r.json(), indent=4, ensure_ascii=False)  # json.dumps()：将结果格式化为json标准格式
+            save_file(filepath, content)
+        else:
+            fail_str = f'name: [{name}], uid: [{uid}], url: [{url}]'
+            log(fail_str, 'fail', 'base_fail.log')
+    except Exception as e:
+        log(traceback.format_exc(), 'error', 'base_error.log')  # traceback.format_exc()：打印出详细错误信息
+        error_str = f'name: [{name}], uid: [{uid}]'
+        log(error_str, 'error', 'base_error.log')
+
+
+def get_video_barrage(d, name, aid):
+    """
+    获取某个视频的弹幕信息
+    :param d:
+    :param name:
+    :param aid:
+    :return:
+    """
+    cid = d['cid']  # 弹幕id
+    barrage_url = f'https://api.bilibili.com/x/v1/dm/list.so?oid={cid}'
+    r = get_http_session().get(barrage_url, timeout=10)
+
+    # 弹幕->xml
+    uid_dir_path = os.path.join(dir_path, name)
+    if not os.path.exists(uid_dir_path):
+        os.makedirs(uid_dir_path)
+    barrage_path = os.path.join(uid_dir_path, f'barrage_{aid}.xml')
+    r.encoding = 'utf-8'
+    content = r.text
+    save_file(barrage_path, content)
+    print(f'video id:{aid} barrage save success')
+
+
+def get_video_comment_info(aid, name):
+    """
+    获取视频评论信息
+    :param aid:
+    :param name:
+    :return:
+    """
+    comment_url = f'https://api.bilibili.com/x/v2/reply?jsonp=jsonp&type=1&oid={aid}&sort=1'
+    r = get_http_session().get(comment_url, timeout=10)
+    if r.status_code == 200:
+        uid_dir_path = os.path.join(dir_path, name)
+        if not os.path.exists(uid_dir_path):
+            os.makedirs(uid_dir_path)
+        comment_path = os.path.join(uid_dir_path, f'comment_{aid}.json')
+        rjson = r.json()  # 返回响应的json，这里是字典类型
+        # dumps 将dict->json
+        content = json.dumps(rjson, indent=4, ensure_ascii=False)
+        save_file(comment_path, content)
+        print(f'video id:{aid} comment save success')
+
+
+def gt_up_video_info(name, uid, filepath):
+    '''
+    获取视频相关信息
+    :param name:
+    :param uid:
+    :param filepath:
+    :return:
+    '''
+    res = read_json(filepath)
+    vlist = res['data']['list']['vlist']  # 视频列表
+    for v in vlist:
+        aid = v['aid']  # 视频id
+        url = f'https://api.bilibili.com/x/player/pagelist?aid={aid}&jsonp=jsonp'
+        player = get_http_session().get(url, timeout=10)
+        player = player.json()
+        data = player['data']  # 弹幕基本信息
+        if not data:
+            return
+        for d in data:
+            try:
+                # get_video_barrage(d, name, aid)
+                get_video_comment_info(aid, name)
+
+            except Exception as e:
+                log(traceback.format_exc(), 'error', 'gt_up_video_info.log')  # traceback.format_exc()：打印出详细错误信息
+                error_str = f'name: [{name}], uid: [{uid}]'
+                log(error_str, 'error', 'gt_up_video_info.log')
+
+
+
+def main():
+    # power_json = read_json('power_up_100.json')
+    # base_info_task(power_json)
+    # 获取视频弹幕信息
+    gt_up_video_info('潮汕好男人', '19071708', 'up_100\\潮汕好男人\\19071708_base_info.json')
+
+
+if __name__ == '__main__':
+    main()
