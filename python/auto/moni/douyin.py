d5a868b3cfc40b82e654c064e9a5a1ef67b34f87
longxiangfu <1565267514@qq.com>
2020-12-11 17:04:55 +0800

    电影数据分析

---
+++ python/auto/moni/douyin.py
@@ -0,0 +1,20 @@
+# """
+# ×Ô¶¯Ë¢¶¶Òô
+# """
+#
+# import time
+# import random
+# from com.android.monkeyrunner import MonkeyRunner, MonkeyDevice
+#
+#
+#
+# device = MonkeyRunner.waitForConnection()
+# device.startActivity(component='com.ss.android.ugc.aweme.lite/com.ss.android.ugc.aweme.splash.SplashActivity')
+# MonkeyRunner.sleep(1)
+# print("connected")
+# while True:
+#     second = random.uniform(10, 15)
+#     time.sleep(second)
+# 	device.drag((400,500),(400,100),0.1,10)
+#     MonkeyRunner.sleep(1)
+# 	print("again")
